f0a0b9cb427d7a3498d2110756f853b093fa1875
pappes <pappes@gmail.com>
2022-07-16 15:51:18 +1000

    increase testing coverage

--- my_movie_search/test/imdb_suggestion_query_test.dart
+++ my_movie_search/test/imdb_suggestion_query_test.dart
@@ -6,27 +6,157 @@ import 'package:my_movie_search/movies/web_data_providers/search/imdb_suggestion
 import 'test_data/imdb_suggestion_converter_data.dart';
 import 'test_helper.dart';
 
+Future<Stream<String>> emitUnexpectedJsonPSample(dynamic dummy) {
+  return Future.value(Stream.value('imdbJsonPFunction(null)'));
+}
+
+Future<Stream<String>> emitInvalidJsonPSample(dynamic dummy) {
+  return Future.value(Stream.value('imdbJsonPFunction({not valid json})'));
+}
+
 void main() {
+////////////////////////////////////////////////////////////////////////////////
+  /// Unit tests
+////////////////////////////////////////////////////////////////////////////////
+
+  group('imdb suggestions unit tests', () {
+    // Confirm class description is constructed as expected.
+    test('Run myDataSourceName()', () async {
+      expect(QueryIMDBSuggestions().myDataSourceName(), 'imdbSuggestions');
+    });
+
+    // Confirm URL is constructed as expected.
+    test('Run myConstructURI()', () async {
+      const expectedResult =
+          'https://sg.media-imdb.com/suggests/n/new%20query.json';
+
+      // Invoke the functionality.
+      final actualResult =
+          QueryIMDBSuggestions().myConstructURI('new query').toString();
+
+      // Check the results.
+      expect(actualResult, expectedResult);
+    });
+
+    // Confirm error is constructed as expected.
+    test('Run myYieldError()', () async {
+      const expectedResult = {
+        'source': 'DataSourceType.imdbSuggestions',
+        'title': '[QueryIMDBSuggestions] new query',
+        'type': 'MovieContentType.custom',
+        'related': '{}'
+      };
+
+      // Invoke the functionality.
+      final actualResult =
+          QueryIMDBSuggestions().myYieldError('new query').toMap();
+
+      // Check the results.
+      expect(actualResult, expectedResult);
+    });
+  });
 ////////////////////////////////////////////////////////////////////////////////
   /// Integration tests
 ////////////////////////////////////////////////////////////////////////////////
 
   group('imdb suggestion query', () {
+    // Confirm map can be converted to DTO.
+    test('Run myTransformMapToOutput()', () async {
+      final expectedValue = await expectedDTOList;
+      final imdbSuggestions = QueryIMDBSuggestions();
+
+      // Invoke the functionality and collect results.
+      final actualResult =
+          imdbSuggestions.myTransformMapToOutput({'d': expectedDTOMap});
+
+      // Check the results.
+      expect(
+        actualResult,
+        MovieResultDTOListMatcher(expectedValue),
+        reason: 'Emmitted DTO list ${actualResult.toString()} '
+            'needs to match expected DTO list ${expectedValue.toString()}',
+      );
+    });
+
     // Read IMDB suggestions from a simulated bytestream and convert JSON to dtos.
     test('Run readList()', () async {
       // Set up the test data.
       final expectedValue = await expectedDTOList;
       final queryResult = <MovieResultDTO>[];
-      final _imdbSuggestions = QueryIMDBSuggestions();
+      final imdbSuggestions = QueryIMDBSuggestions();
 
       // Invoke the functionality.
-      await _imdbSuggestions
-          .readList(SearchCriteriaDTO(), source: emitImdbJsonSample)
+      await imdbSuggestions
+          .readList(SearchCriteriaDTO(), source: emitImdbSuggestionJsonPSample)
           .then((values) => queryResult.addAll(values))
           .onError(
             // ignore: avoid_print
             (error, stackTrace) => print('$error, ${stackTrace.toString()}'),
           );
+
+      // Check the results.
+      expect(
+        queryResult,
+        MovieResultDTOListMatcher(expectedValue),
+        reason: 'Emmitted DTO list ${queryResult.toString()} '
+            'needs to match expected DTO list ${expectedValue.toString()}',
+      );
+    });
+
+    // Read IMDB suggestions from a simulated bytestream and convert JSON to dtos.
+    test('invalid jsonp', () async {
+      // Set up the test data.
+      final expectedValue = <MovieResultDTO>[];
+      final queryResult = <MovieResultDTO>[];
+      final imdbSuggestions = QueryIMDBSuggestions();
+      late String actualException;
+      const expectedException = '''
+FormatException: Unexpected character (at character 2)
+{not valid json}
+ ^
+''';
+
+      // Invoke the functionality.
+      await imdbSuggestions
+          .readList(SearchCriteriaDTO(), source: emitInvalidJsonPSample)
+          .then((values) => queryResult.addAll(values))
+          .onError(
+        // ignore: avoid_print
+        (error, stackTrace) {
+          actualException = error.toString();
+        },
+      );
+      expect(actualException, expectedException);
+
+      // Check the results.
+      expect(
+        queryResult,
+        MovieResultDTOListMatcher(expectedValue),
+        reason: 'Emmitted DTO list ${queryResult.toString()} '
+            'needs to match expected DTO list ${expectedValue.toString()}',
+      );
+    });
+
+    // Read IMDB suggestions from a simulated bytestream and convert JSON to dtos.
+    test('unexpected json contents', () async {
+      // Set up the test data.
+      final expectedValue = <MovieResultDTO>[];
+      final queryResult = <MovieResultDTO>[];
+      final imdbSuggestions = QueryIMDBSuggestions();
+      late String actualException;
+      const expectedException = null;
+
+      // Invoke the functionality.
+      await imdbSuggestions
+          .readList(SearchCriteriaDTO(), source: emitUnexpectedJsonPSample)
+          .then((values) => queryResult.addAll(values))
+          .onError(
+        // ignore: avoid_print
+        (error, stackTrace) {
+          actualException = error.toString();
+        },
+      );
+      expect(actualException, expectedException);
 
       // Check the results.
       expect(
--- my_movie_search/test/test_data/imdb_suggestion_converter_data.dart
+++ my_movie_search/test/test_data/imdb_suggestion_converter_data.dart
@@ -24,10 +24,11 @@ Stream<MovieResultDTO> streamFromInnerJsonMap(
 }
 
 final expectedDTOList = expectedDTOStream.toList();
-final expectedDTOStream = streamFromInnerJsonMap([
+final expectedDTOStream = streamFromInnerJsonMap(expectedDTOMap);
+final expectedDTOMap = [
   {
-    "l": "Wonder Woman 1984",
-    "id": "tt7126948",
+    "l": "Imdb Offline Suggestions 1984",
+    "id": "tt7324958",
     "s": "Gal Gadot, Chris Pine",
     "y": 2020,
     "q": "feature",
@@ -40,7 +41,7 @@ final expectedDTOStream = streamFromInnerJsonMap([
     "v": [
       {
         "l": "4K Trailer",
-        "id": "vi3944268057",
+        "id": "vi3142238457",
         "s": "2:31",
         "i": [
           "https://m.media-amazon.com/images/M/MV5B...Rvb2xpbmhk._V1_.jpg",
@@ -50,7 +51,7 @@ final expectedDTOStream = streamFromInnerJsonMap([
       },
       {
         "l": "Opening Scene",
-        "id": "vi321831193",
+        "id": "vi311233143",
         "s": "3:26",
         "i": [
           "https://m.media-amazon.com/images/M/MV5B...QWRvb2xpbmhk._V1_.jpg",
@@ -59,8 +60,8 @@ final expectedDTOStream = streamFromInnerJsonMap([
         ]
       },
       {
-        "l": "Wonder Woman 1984",
-        "id": "vi2517680409",
+        "l": "Imdb Offline Suggestions 1984",
+        "id": "vi2112630409",
         "s": "1:32",
         "i": [
           "https://m.media-amazon.com/images/M/MV5B...Zmxvdw@@._V1_.jpg",
@@ -71,8 +72,8 @@ final expectedDTOStream = streamFromInnerJsonMap([
     ]
   },
   {
-    "l": "Wonder Woman",
-    "id": "tt0451279",
+    "l": "Imdb Offline Suggestions",
+    "id": "tt0354259",
     "s": "Gal Gadot, Chris Pine",
     "y": 2017,
     "q": "feature",
@@ -84,8 +85,8 @@ final expectedDTOStream = streamFromInnerJsonMap([
     ],
     "v": [
       {
-        "l": "Rise of the Warrior",
-        "id": "vi1553381657",
+        "l": "Rise of the Suggestion",
+        "id": "vi1152331457",
         "s": "2:36",
         "i": [
           "https://m.media-amazon.com/images/M/MV5B...NzZXI@._V1_.jpg",
@@ -94,8 +95,8 @@ final expectedDTOStream = streamFromInnerJsonMap([
         ]
       },
       {
-        "l": "Meet Maxwell Lord: The 'Wonder Woman 1984' Big Bad",
-        "id": "vi237027609",
+        "l": "Meet Maxwell Lord: The 'Imdb Offline Suggestions 1984' Big Bad",
+        "id": "vi217223649",
         "s": "3:57",
         "i": [
           "https://m.media-amazon.com/images/M/MV5B...ZWxvZw@@._V1_.jpg",
@@ -104,8 +105,8 @@ final expectedDTOStream = streamFromInnerJsonMap([
         ]
       },
       {
-        "l": "Official Origin Trailer",
-        "id": "vi1901311513",
+        "l": "Official Suggestion Trailer",
+        "id": "vi1102331413",
         "s": "2:30",
         "i": [
           "https://m.media-amazon.com/images/M/MV5B...TI2MTU@._V1_.jpg",
@@ -116,8 +117,8 @@ final expectedDTOStream = streamFromInnerJsonMap([
     ]
   },
   {
-    "l": "Wonder Woman",
-    "id": "tt1740828",
+    "l": "Imdb Offline Suggestions",
+    "id": "tt1344858",
     "s": "Pedro Pascal, Adrianne Palicki",
     "y": 2011,
     "q": "TV movie",
@@ -128,8 +129,8 @@ final expectedDTOStream = streamFromInnerJsonMap([
     ]
   },
   {
-    "l": "Wonder Woman",
-    "id": "tt0074074",
+    "l": "Imdb Offline Suggestions",
+    "id": "tt0374054",
     "s": "Lynda Carter, Lyle Waggoner",
     "y": 1975,
     "yr": "1975-1979",
@@ -141,8 +142,8 @@ final expectedDTOStream = streamFromInnerJsonMap([
     ]
   },
   {
-    "l": "Wonder Woman: Bloodlines",
-    "id": "tt8752498",
+    "l": "Imdb Offline Suggestions: Bloodlines",
+    "id": "tt8354458",
     "s": "Rosario Dawson, Jeffrey Donovan",
     "y": 2019,
     "q": "feature",
@@ -153,8 +154,8 @@ final expectedDTOStream = streamFromInnerJsonMap([
     ]
   },
   {
-    "l": "Wonder Woman",
-    "id": "tt0072419",
+    "l": "Imdb Offline Suggestions",
+    "id": "tt0374459",
     "s": "Cathy Lee Crosby, Kaz Garas",
     "y": 1974,
     "q": "TV movie",
@@ -165,8 +166,8 @@ final expectedDTOStream = streamFromInnerJsonMap([
     ]
   },
   {
-    "l": "Wonder Woman",
-    "id": "tt1186373",
+    "l": "Imdb Offline Suggestions",
+    "id": "tt1384353",
     "s": "Keri Russell, Nathan Fillion",
     "y": 2009,
     "q": "video",
@@ -178,7 +179,7 @@ final expectedDTOStream = streamFromInnerJsonMap([
   },
   {
     "l": "Jennifer Wenger",
-    "id": "nm2628854",
+    "id": "nm2128254",
     "s": "Actress, Jimmy Kimmel Live! (2006-2007)",
     "i": [
       "https://m.media-amazon.com/images/M/MV5B...cxNQ@@._V1_.jpg",
@@ -186,47 +187,47 @@ final expectedDTOStream = streamFromInnerJsonMap([
       428
     ]
   }
-]);
+];
 
 const String imdbJsonSampleInner = '''
-  {"l":"Wonder Woman 1984","id":"tt7126948","s":"Gal Gadot, Chris Pine","y":2020,"q":"feature","vt":35
+  {"l":"Imdb Offline Suggestions 1984","id":"tt7324958","s":"Gal Gadot, Chris Pine","y":2020,"q":"feature","vt":35
       ,"i":["https://m.media-amazon.com/images/M/MV5B...Q3MTUy._V1_.jpg",2764,4096]
       ,"v":
       [
-          {"l":"4K Trailer","id":"vi3944268057","s":"2:31","i":["https://m.media-amazon.com/images/M/MV5B...xpbmhk._V1_.jpg",1404,790]},
-          {"l":"Opening Scene","id":"vi321831193","s":"3:26","i":["https://m.media-amazon.com/images/M/MV5B...pbmhk._V1_.jpg",1343,756]},
-          {"l":"Wonder Woman 1984","id":"vi2517680409","s":"1:32","i":["https://m.media-amazon.com/images/M/MV5B...xvdw@@._V1_.jpg",1920,1080]}
+          {"l":"4K Trailer","id":"vi3142238457","s":"2:31","i":["https://m.media-amazon.com/images/M/MV5B...xpbmhk._V1_.jpg",1404,790]},
+          {"l":"Opening Scene","id":"vi311233143","s":"3:26","i":["https://m.media-amazon.com/images/M/MV5B...pbmhk._V1_.jpg",1343,756]},
+          {"l":"Imdb Offline Suggestions 1984","id":"vi2112630409","s":"1:32","i":["https://m.media-amazon.com/images/M/MV5B...xvdw@@._V1_.jpg",1920,1080]}
       ]},
-  {"l":"Wonder Woman","id":"tt0451279","s":"Gal Gadot, Chris Pine","y":2017,"q":"feature","vt":23
+  {"l":"Imdb Offline Suggestions","id":"tt0354259","s":"Gal Gadot, Chris Pine","y":2017,"q":"feature","vt":23
       ,"i":["https://m.media-amazon.com/images/M/MV5B...zE3OTE@._V1_.jpg",2025,3000]
       ,"v":
       [
-          {"l":"Rise of the Warrior","id":"vi1553381657","s":"2:36"
+          {"l":"Rise of the Suggestion","id":"vi1152331457","s":"2:36"
               ,"i":["https://m.media-amazon.com/images/M/MV5B...zZXI@._V1_.jpg",1492,788]},
-          {"l":"Meet Maxwell Lord: The 'Wonder Woman 1984' Big Bad","id":"vi237027609","s":"3:57"
+          {"l":"Meet Maxwell Lord: The 'Imdb Offline Suggestions 1984' Big Bad","id":"vi217223649","s":"3:57"
               ,"i":["https://m.media-amazon.com/images/M/MV5B...xvZw@@._V1_.jpg",1920,1080]},
-          {"l":"Official Origin Trailer","id":"vi1901311513","s":"2:30","i":["https://m.media-amazon.com/images/M/MV5B...OTI2MTU@._V1_.jpg",1280,720]}
+          {"l":"Official Suggestion Trailer","id":"vi1102331413","s":"2:30","i":["https://m.media-amazon.com/images/M/MV5B...OTI2MTU@._V1_.jpg",1280,720]}
       ]},
-  {"l":"Wonder Woman","id":"tt1740828","s":"Pedro Pascal, Adrianne Palicki","y":2011,"q":"TV movie"
+  {"l":"Imdb Offline Suggestions","id":"tt1344858","s":"Pedro Pascal, Adrianne Palicki","y":2011,"q":"TV movie"
       ,"i":["https://m.media-amazon.com/images/M/MV5B...DM4NA@@._V1_.jpg",761,1800]},
-  {"l":"Wonder Woman","id":"tt0074074","s":"Lynda Carter, Lyle Waggoner","y":1975,"yr":"1975-1979","q":"TV series"
+  {"l":"Imdb Offline Suggestions","id":"tt0374054","s":"Lynda Carter, Lyle Waggoner","y":1975,"yr":"1975-1979","q":"TV series"
       ,"i":["https://m.media-amazon.com/images/M/MV5B...ODE1MDc@._V1_.jpg",702,998]},
-  {"l":"Wonder Woman: Bloodlines","id":"tt8752498","s":"Rosario Dawson, Jeffrey Donovan","y":2019,"q":"feature"
+  {"l":"Imdb Offline Suggestions: Bloodlines","id":"tt8354458","s":"Rosario Dawson, Jeffrey Donovan","y":2019,"q":"feature"
       ,"i":["https://m.media-amazon.com/images/M/MV5BZ...UxMzE@._V1_.jpg",1365,2048]},
-  {"l":"Wonder Woman","id":"tt0072419","s":"Cathy Lee Crosby, Kaz Garas","y":1974,"q":"TV movie"
+  {"l":"Imdb Offline Suggestions","id":"tt0374459","s":"Cathy Lee Crosby, Kaz Garas","y":1974,"q":"TV movie"
       ,"i":["https://m.media-amazon.com/images/M/MV5B...kwMDE@._V1_.jpg",353,500]},
-  {"l":"Wonder Woman","id":"tt1186373","s":"Keri Russell, Nathan Fillion","y":2009,"q":"video"
+  {"l":"Imdb Offline Suggestions","id":"tt1384353","s":"Keri Russell, Nathan Fillion","y":2009,"q":"video"
       ,"i":["https://m.media-amazon.com/images/M/MV5B...xODE1MDc@._V1_.jpg",500,741]},
-  {"l":"Jennifer Wenger","id":"nm2628854","s":"Actress, Jimmy Kimmel Live! (2006-2007)"
+  {"l":"Jennifer Wenger","id":"nm2128254","s":"Actress, Jimmy Kimmel Live! (2006-2007)"
       ,"i":["https://m.media-amazon.com/images/M/MV5B...cxNQ@@._V1_.jpg",640,428]}
 ''';
 const String imdbJsonPFunction = r'imdb$wonder_woman';
 const String imdbCustomKeyName = 'Cust';
 const String imdbCustomKeyVal = 'jsonpPTest';
 const String imdbJsonSampleOuter =
-    '{"v":1,"q":"wonder_woman","d":[ $imdbJsonSampleInner ],"$imdbCustomKeyName":"$imdbCustomKeyVal"}';
+    '{"v":1,"q":"imdb_offline_suggestions","d":[ $imdbJsonSampleInner ],"$imdbCustomKeyName":"$imdbCustomKeyVal"}';
 const String imdbJsonPSampleFull = '$imdbJsonPFunction($imdbJsonSampleOuter)';
 
-Future<Stream<String>> emitImdbJsonSample(dynamic dummy) {
+Future<Stream<String>> emitImdbSuggestionJsonPSample(dynamic dummy) {
   return Future.value(Stream.value(imdbJsonPSampleFull));
 }
