2fc16c08adad2d441d29d9187ea8bc098ed9235f
pappes <pappes@gmail.com>
2023-07-07 22:18:12 +1000

    #64 shown dirlldown actions as buttons

--- my_movie_search/lib/movies/screens/widgets/movie_card_small.dart
+++ my_movie_search/lib/movies/screens/widgets/movie_card_small.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart'
     show
         BuildContext,
+        ElevatedButton,
         Icon,
-        IconData,
         Icons,
         Image,
         ListTile,
         NetworkImage,
+        SizedBox,
         Text,
         Widget;
 
@@ -18,13 +19,16 @@ import 'package:my_movie_search/utilities/extensions/enum.dart';
 import 'package:my_movie_search/utilities/navigation/web_nav.dart';
 
 class MovieTile extends ListTile {
-  MovieTile(BuildContext context, MovieResultDTO movie)
+  MovieTile(this.context, this.movie)
       : super(
           leading: _getImage(movie),
           title: _getTitle(movie),
+          trailing: _getButton(context, movie),
           subtitle: _getDescription(movie),
-          onTap: () => MMSNav(context).resultDrillDown(movie),
+          onTap: () => _navigate(context, movie),
         );
+  final BuildContext context;
+  final MovieResultDTO movie;
 
   static Widget _getTitle(MovieResultDTO movie) {
     var year = '';
@@ -108,6 +112,8 @@ class MovieTile extends ListTile {
         return const Icon(Icons.skip_next);
       case MovieContentType.person:
         return const Icon(Icons.person);
+      case MovieContentType.keyword:
+        return const Icon(Icons.manage_search);
       case MovieContentType.download:
         return movie.imageUrl == ''
             ? const Icon(Icons.block)
@@ -117,5 +123,33 @@ class MovieTile extends ListTile {
             ? Image(image: NetworkImage(movie.imageUrl))
             : const Icon(Icons.theaters);
     }
+  }
+
+  static Widget _getButton(BuildContext context, MovieResultDTO movie) {
+    switch (movie.type) {
+      case MovieContentType.navigation:
+        return _navigateButton(context, movie);
+      case MovieContentType.keyword:
+        return _navigateButton(context, movie);
+      case MovieContentType.download:
+        return movie.imageUrl == ''
+            ? const SizedBox.shrink()
+            : _navigateButton(context, movie);
+      default:
+        return const Text('');
+    }
+  }
+
+  static void _navigate(BuildContext context, MovieResultDTO movie) =>
+      MMSNav(context).resultDrillDown(movie);
+
+  static ElevatedButton _navigateButton(
+    BuildContext context,
+    MovieResultDTO movie,
+  ) {
+    return ElevatedButton(
+      onPressed: () => _navigate(context, movie),
+      child: _getImage(movie),
+    );
   }
 }
