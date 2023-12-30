7bd91c046efdbc3e104ea0ba1849067ccfeea682
pappes <pappes@gmail.com>
2023-12-30 10:52:45 +1000

    fix broken test

--- my_movie_search/lib/movies/web_data_providers/detail/offline/imdb_name.dart
+++ my_movie_search/lib/movies/web_data_providers/detail/offline/imdb_name.dart
@@ -15,12 +15,14 @@ Future<Stream<String>> streamImdbHtmlOfflineData(_) =>
 in test('Run dtoFromCompleteJsonMap()'*/
 const expectedDtoJsonStringList = [
   r'''
-{"uniqueId":"nm0123456","bestSource":"DataSourceType.imdb","title":"Mescalon Smoochington <3","type":"MovieContentType.person","year":"1933","yearRange":"1933-1977","languages":"[]","genres":"[]","keywords":"[]",
+{"uniqueId":"nm0123456","bestSource":"DataSourceType.imdb","title":"Mescalon Smoochington <3","type":"MovieContentType.person","year":"1933","yearRange":"1933-1977",
       "description":"THen Kramer said, \"Everybody is Mescalon Smoochington\".","userRatingCount":"184","imageUrl":"https://www.microsoft.com/images/M/MV5BNjdhNz.jpg","sources":{"DataSourceType.imdb":"nm0123456"},
-  "related":{"Actor":{"tt0012370":{"uniqueId":"tt0012370","bestSource":"DataSourceType.imdbSuggestions","title":"Walk Skip Run","alternateTitle":" Run Forrest Run","charactorName":" [Willy Rutter]","type":"MovieContentType.title","year":"1973","yearRange":"1973","runTime":"7140","languages":"[]",
-      "genres":"[\"Western\",\"Romance\"]","keywords":"[]","userRating":"8.6","userRatingCount":"4837","imageUrl":"https://www.microsoft.com/images/M/MV5BM.jpg","sources":{"DataSourceType.imdbSuggestions":"tt0012370"},"related":{}},
-      "tt0123580":{"uniqueId":"tt0123580","bestSource":"DataSourceType.imdbSuggestions","title":"Scott And Sharlene","alternateTitle":" Aussieland","charactorName":" [Nom da Plume, Other Charactor]","type":"MovieContentType.short","year":"1985","yearRange":"1985-2023","runTime":"1234","languages":"[]",
-      "genres":"[\"Horror\",\"Romance\"]","keywords":"[]","userRating":"7.5","userRatingCount":"5123","censorRating":"CensorRatingType.family","imageUrl":"https://www.microsoft.com/images/M/MV5BYjAxMz.jpg","sources":{"DataSourceType.imdbSuggestions":"tt0123580"},"related":{}}}}}
+  "related":{"Actor":{"tt0012370":{"uniqueId":"tt0012370","bestSource":"DataSourceType.imdbSuggestions","title":"Walk Skip Run","alternateTitle":" Run Forrest Run","charactorName":" [Willy Rutter]","type":"MovieContentType.movie","year":"1973","yearRange":"1973","runTime":"7140",
+      "genres":"[\"Western\",\"Romance\"]",
+      "userRating":"8.6","userRatingCount":"4837","imageUrl":"https://www.microsoft.com/images/M/MV5BM.jpg","sources":{"DataSourceType.imdbSuggestions":"tt0012370"}},
+      "tt0123580":{"uniqueId":"tt0123580","bestSource":"DataSourceType.imdbSuggestions","title":"Scott And Sharlene","alternateTitle":" Aussieland","charactorName":" [Nom da Plume, Other Charactor]","type":"MovieContentType.short","year":"1985","yearRange":"1985-2023","runTime":"1234",
+      "genres":"[\"Horror\",\"Romance\"]",
+      "userRating":"7.5","userRatingCount":"5123","censorRating":"CensorRatingType.family","imageUrl":"https://www.microsoft.com/images/M/MV5BYjAxMz.jpg","sources":{"DataSourceType.imdbSuggestions":"tt0123580"}}}}}
 ''',
 ];
 
